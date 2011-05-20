8710f1519f4c536f0299c44acc45ec2f33b8ad24
Brent Faulkner <brent.faulkner@mosaic.com>
2011-05-19 23:14:05 -0400

    not why's hpricot

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-config.gem 'hpricot', :source => 'http://code.whytheluckystiff.net'
+config.gem 'hpricot'
 config.gem 'sbfaulkner-pygmalion', :lib => 'pygmalion', :source => 'http://gems.github.com'
 
 ActiveRecord::Base.send :include, HasMarkup
